738b0395e452d436460128e6118b1e4980834649
Rachie94 <145667840+Rachie94@users.noreply.github.com>
2024-04-18 15:56:45 +0100

    Update MyJavaScript.js

--- templates/MyJavaScript.js
+++ templates/MyJavaScript.js
@@ -20,3 +20,48 @@ document.getElementById('generate_button').addEventListener('click', function() 
     // Show the copy button when the generate button is clicked
     document.getElementById('copy_button').style.display = 'inline';
 });
+
+function displayMessages(messages, messageType) {
+    // Clear existing messages
+    const messageContainer = document.getElementById('message-container');
+    messageContainer.innerHTML = '';
+
+    // Create new message elements and append to the container
+    messages.forEach(message => {
+        const messageElement = document.createElement('p');
+        messageElement.textContent = message;
+        messageElement.classList.add(messageType);
+        messageContainer.appendChild(messageElement);
+    });
+}
+
+<script>
+      // auto_logout.js
+
+var timeout;
+
+function startLogoutTimer() {
+    timeout = setTimeout(function() {
+        window.location.href = '/logout'; // Redirect to logout route on timeout
+    }, 60 * 1000); // Timeout after 1 minute (in milliseconds)
+}
+
+function resetLogoutTimer() {
+    clearTimeout(timeout); // Reset timer
+    startLogoutTimer(); // Start timer again
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    startLogoutTimer(); // Start timer when the page loads
+
+    // Reset timer on user interaction
+    document.addEventListener('mousemove', resetLogoutTimer);
+    document.addEventListener('keydown', resetLogoutTimer);
+    // Add more events as needed (e.g., click, touch)
+});
+
+<script>
+document.getElementById("newItemButton").addEventListener("click", function() {
+    window.location.href = "/new_item";
+});
+</script>
