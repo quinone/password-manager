d2b863cd041cce8eb0514afb923de49e3f0f055d
Rachie94 <145667840+Rachie94@users.noreply.github.com>
2024-04-26 21:02:32 +0100

    Update database.py

--- database.py
+++ database.py
@@ -1,184 +1,122 @@
 import sqlite3
+from sqlite3 import Error
 
-# Connect to the database (will create it if not exists)
-conn = sqlite3.connect("CAPSTONE-PROJECT.db")
-cursor = conn.cursor()
+db_file = "CAPSTONE-PROJECT.db"
 
-# Create REGISTRATION table with default values
-cursor.execute(
-    """
-CREATE TABLE IF NOT EXISTS REGISTRATION (
-    ID INTEGER PRIMARY KEY,
-    EMAIL VARCHAR(250),
-    NAME VARCHAR(250),
-    MASTER_PASSWORD VARCHAR(250),
-    PASSWORD_HINT VARCHAR(250),
-    PREFERENCES_ID INTEGER,
-    EMAIL_CONFIRMED BOOLEAN DEFAULT 'N'
-)
-"""
-)
+def connect(db_file):
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except sqlite3.Error as e:
+        print(e)
+        return None
 
-# Create LOGIN table
-cursor.execute(
-    """
-CREATE TABLE IF NOT EXISTS LOGIN (
-    ID INTEGER PRIMARY KEY,
-    EMAIL VARCHAR(250),
-    LOGIN_ATTEMPTS INTEGER CHECK (LOGIN_ATTEMPTS <= 3) DEFAULT 0
-)
-"""
-)
+def create_connection(db_file):
+    """ create a database connection to a SQLite database """
 
-# Create ITEM table
-cursor.execute(
-    """
-CREATE TABLE IF NOT EXISTS ITEM (
-    ID INTEGER PRIMARY KEY,
-    ITEM_TYPE_ID INTEGER,
-    NAME VARCHAR(250),
-    FOLDER_ID INTEGER,
-    USERNAME VARCHAR(250),
-    PASSWORD VARCHAR(250),
-    URI VARCHAR(250),
-    NOTES VARCHAR(250),
-    CUSTOM_FIELD_NAME VARCHAR(255),
-    CUSTOM_FIELD_TYPE INTEGER,
-    CUSTOM_FIELD_VALUE VARCHAR(250)
-)
-"""
-)
+    try:
+        conn = connect(db_file)
+        if conn is not None:
+            cursor = conn.cursor()
 
-# Create ITEM_TYPE table with default values if it doesn't exist
-cursor.execute(
-    """
-CREATE TABLE IF NOT EXISTS ITEM_TYPE (
-    ID INTEGER PRIMARY KEY AUTOINCREMENT,
-    ITEM_TYPE VARCHAR(250) 
-)
-"""
-)
+            cursor.execute('''CREATE TABLE IF NOT EXISTS REGISTRATION (
+                      ID INTEGER PRIMARY KEY,
+                      USER_ID INTEGER UNIQUE,
+                      EMAIL VARCHAR(250) UNIQUE,
+                      NAME VARCHAR(250),
+                      MASTER_PASSWORD VARCHAR(250),
+                      PASSWORD_HINT VARCHAR(250),
+                      PREFERENCES_ID INTEGER,
+                      EMAIL_CONFIRMED BOOLEAN DEFAULT 'N',
+                      FOREIGN KEY (USER_ID) REFERENCES LOGIN(ID) ON DELETE CASCADE,
+                      FOREIGN KEY (PREFERENCES_ID) REFERENCES PREFERENCES(ID) ON DELETE CASCADE
+                  ) ''')
 
-# Check if the ITEM_TYPE table is empty
-cursor.execute("SELECT COUNT(*) FROM ITEM_TYPE")
-count = cursor.fetchone()[0]
+            cursor.execute('''
+                    CREATE TABLE IF NOT EXISTS LOGIN (
+                          ID INTEGER PRIMARY KEY,
+                          EMAIL VARCHAR(250) UNIQUE,
+                          PASSWORD_HASH VARCHAR(250)
+                      )
+                      ''')
 
-# Insert default values into the ITEM_TYPE table only if it's empty
-if count == 0:
-    default_types = ["login", "Card", "Identity", "SecureNote"]
-    for item_type in default_types:
-        cursor.execute("INSERT INTO ITEM_TYPE (ITEM_TYPE) VALUES (?)", (item_type,))
+            cursor.execute('''
+            CREATE TABLE IF NOT EXISTS FOLDER (
+                      ID INTEGER PRIMARY KEY,
+                      USER_ID INTEGER,
+                      FOLDER_NAME VARCHAR(250),
+                      FOREIGN KEY (USER_ID) REFERENCES LOGIN(ID) ON DELETE CASCADE
+                  ) ''')
 
-# Create FOLDER table
-cursor.execute(
-    """
-CREATE TABLE IF NOT EXISTS FOLDER (
-    ID INTEGER PRIMARY KEY,
-    FOLDER_NAME VARCHAR(250)
-)
-"""
-)
+            cursor.execute('''CREATE TABLE IF NOT EXISTS ITEM (
+                          ID INTEGER PRIMARY KEY,
+                          USER_ID INTEGER,
+                          FOLDER_ID INTEGER,
+                          ITEM_TYPE_ID INTEGER,
+                          NAME VARCHAR(250),
+                          USERNAME VARCHAR(250),
+                          PASSWORD VARCHAR(250),
+                          URI VARCHAR(250),
+                          NOTES VARCHAR(250),
+                          CUSTOM_FIELD_NAME VARCHAR(255),
+                          CUSTOM_FIELD_TYPE INTEGER,
+                          CUSTOM_FIELD_VALUE VARCHAR(250),
+                          FOREIGN KEY (USER_ID) REFERENCES LOGIN(ID) ON DELETE CASCADE,
+                          FOREIGN KEY (FOLDER_ID) REFERENCES FOLDER(ID) ON DELETE CASCADE
+                      ) ''')
 
-# Create DANGER_ZONE table
-cursor.execute(
-    """
-CREATE TABLE IF NOT EXISTS DANGER_ZONE (
-    ID INTEGER PRIMARY KEY,
-    DEAUTHORISED_SESSION BOOLEAN,
-    PURGED BOOLEAN,
-    DELETED_ACCOUNT BOOLEAN,
-    TIMESTAMP DATETIME,
-    OWNER VARCHAR(250)
-)
-"""
-)
+            cursor.execute('''CREATE TABLE IF NOT EXISTS ITEM_TYPE (
+                          ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                          ITEM_TYPE VARCHAR(250) 
+                      )''')
 
-# Create AUDIT table
-cursor.execute(
-    """
-CREATE TABLE IF NOT EXISTS AUDIT (
-    ID INTEGER PRIMARY KEY,
-    ENTITY_TYPE_ID VARCHAR(50),
-    ENTITY_ID INTEGER,
-    ACTION_TYPE VARCHAR(50),
-    USER_ID INTEGER,
-    TIMESTAMP DATETIME
-)
-"""
-)
+            cursor.execute(''' CREATE TABLE IF NOT EXISTS PREFERENCES (
+                          ID INTEGER PRIMARY KEY,
+                          USER_ID INTEGER,
+                          VAULT_TIMEOUT TIME,
+                          THEME_ID INTEGER,
+                          LANGUAGE_ID INTEGER,
+                          FOREIGN KEY (USER_ID) REFERENCES LOGIN(ID) ON DELETE CASCADE
+                      )''')
 
-# Create PREFERENCES table
-cursor.execute(
-    """
-CREATE TABLE IF NOT EXISTS PREFERENCES (
-    ID INTEGER PRIMARY KEY,
-    VAULT_TIMEOUT TIME,
-    THEME_ID INTEGER,
-    LANGUAGE_ID INTEGER
-)
-"""
-)
+            cursor.execute('''CREATE TABLE IF NOT EXISTS AUDIT (
+                          ID INTEGER PRIMARY KEY,
+                          ENTITY_TYPE_ID VARCHAR(50),
+                          ENTITY_ID INTEGER,
+                          ACTION_TYPE VARCHAR(50),
+                          USER_ID INTEGER,
+                          TIMESTAMP DATETIME,
+                          FOREIGN KEY (USER_ID) REFERENCES LOGIN(ID) ON DELETE CASCADE
+                      )''')
 
-# Create AUDIT_ITEM_TYPE table if it doesn't exist
-cursor.execute(
-    """
-CREATE TABLE IF NOT EXISTS AUDIT_ITEM_TYPE (
-    ID INTEGER PRIMARY KEY AUTOINCREMENT,
-    TYPE VARCHAR(250)
-)
-"""
-)
-
-# Check if the AUDIT_ITEM_TYPE table is empty
-cursor.execute("SELECT COUNT(*) FROM AUDIT_ITEM_TYPE")
-count = cursor.fetchone()[0]
-
-# Insert default values into the AUDIT_ITEM_TYPE table only if it's empty
-if count == 0:
-    default_types = ["REGISTRATION", "LOGIN", "VAULT", "VAULT_ITEM", "CHANGED_PASSWORD"]
-    for item_type in default_types:
-        cursor.execute("INSERT INTO AUDIT_ITEM_TYPE (TYPE) VALUES (?)", (item_type,))
+    except sqlite3.Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
 
 
-# Create VAULT_THEME table with default values
-cursor.execute(
-    """
-CREATE TABLE IF NOT EXISTS VAULT_THEME (
-    ID INTEGER PRIMARY KEY,
-    THEME VARCHAR(250) DEFAULT 'DARK, STANDARD'
-)
-"""
-)
 
-# Check if the VAULT_THEME table is empty
-cursor.execute("SELECT COUNT(*) FROM VAULT_THEME")
-count = cursor.fetchone()[0]
+def displayData():
+    """ create a database connection to a SQLite database """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        cursor = conn.cursor()
+        data = cursor.execute('''SELECT * FROM FOLDER ''')
 
-# Insert default values into the VAULT_THEME table only if it's empty
-if count == 0:
-    default_themes = ["Dark", "Standard"]
-    for theme in default_themes:
-        cursor.execute("INSERT INTO VAULT_THEME (THEME) VALUES (?)", (theme,))
+        print("Data in the table FOLDER: ")
+        for row in data:
+            print(row)
+    except Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
 
 
-# Create LANGUAGES table with default values
-cursor.execute(
-    """
-CREATE TABLE IF NOT EXISTS LANGUAGES (
-    ID INTEGER PRIMARY KEY,
-    LANGUAGE VARCHAR(250) 
-)
-"""
-)
-# Check if the LANGUAGES table is empty
-cursor.execute("SELECT COUNT(*) FROM LANGUAGES")
-count = cursor.fetchone()[0]
+if __name__ == '__main__':
+ displayData()
+   # create_connection("CAPSTONE-PROJECT.db")
 
-# Insert default values into the LANGUAGES table only if it's empty
-if count == 0:
-    default_languages = ["English", "Spanish", "French", "Italian", "Irish"]
-    for language in default_languages:
-        cursor.execute("INSERT INTO LANGUAGES (LANGUAGE) VALUES (?)", (language,))
-# Commit changes and close connection
-conn.commit()
-conn.close()
+
