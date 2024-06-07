aa59e3dfc8b40e2acdfb2addb766f873ca10a3b9
Pitoche <adeluis0@gmail.com>
2024-06-07 14:03:46 +0100

    Working with tests and Password generator

--- app/PassGenerator.py
+++ app/PassGenerator.py
@@ -27,6 +27,39 @@ from app.forms import SearchForm
 
 bootstrap = Bootstrap()
 
+def generate_password(
+        length, min_length, min_numbers, min_special_chars, special_chars, avoid_ambiguous
+):
+    password = ""
+    numbers = string.digits
+    special_characters = "".join(special_chars)
+    ambiguous_characters = "0Oo1Il|"
+    if avoid_ambiguous:
+        characters = "".join(
+            [
+                c
+                for c in string.ascii_letters + numbers + special_characters
+                if c not in ambiguous_characters
+            ]
+        )
+    else:
+        characters = string.ascii_letters + numbers + special_characters
+
+    # Add minimum numbers
+    for _ in range(min_numbers):
+        password += random.choice(numbers)
+
+    # Add minimum special characters
+    for _ in range(min_special_chars):
+        password += random.choice(special_characters)
+
+    # Generate the rest of the password
+    remaining_length = length - len(password)
+    for _ in range(remaining_length):
+        password += random.choice(characters)
+
+    password = "".join(random.sample(password, len(password)))
+    return password
 
 def create_app(test_config=None, vault=None):
     # Create and configure the app
@@ -78,8 +111,6 @@ def create_app(test_config=None, vault=None):
 
     # Set the session lifetime
     app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(minutes=1)
-
-
 
     @app.before_request
     @login_required
@@ -142,6 +173,7 @@ def create_app(test_config=None, vault=None):
                 special_chars = request.form.getlist("special_chars")
 
                 if password_type == "Alphanumeric":
+                    # If no special characters
                     # If no special characters are selected, show a Flask message
                     if not special_chars:
                         flash("Please select some special characters.")
@@ -160,111 +192,72 @@ def create_app(test_config=None, vault=None):
                     characters = string.digits
                     password = generate_password(total_length, min_length, min_length, min_length, characters, False)
 
-        password = generate_password(
-            length,
-            min_length,
-            min_numbers,
-            min_special_chars,
-            special_chars,
-            avoid_ambiguous,
-        )
-        return jsonify(password=password)
-
-    @app.route("/", methods=["GET", "POST"])
-    @login_required
-    def index():
-        if request.method == "POST":
-            return handle_generate_password()
-
-        return render_template("index.html")
-
-    @app.route("/gen_Password")
-    @login_required
-    def gen_Password():
-        return render_template("gen_Password.html")
-
-
-
-
-    @app.route("/password_generator")
-    @login_required
-    def password_generator():
-        return render_template("Password_generator.html")
-
-    @app.route("/settings")
-    def settings():
-        return render_template("settings.html")
-
-    # Add this route to handle account deletion// not deleting data related to user just user profile
-    @app.route("/delete_account", methods=["POST"])
-    @login_required
-    def delete_account():
-        # Check if the user is authenticated
-        if "user_id" in session:
-            user_id = session["user_id"]
-            conn = db.get_db()
-            try:
-                # Connect to the database
-                # conn = database.connect(db_file)
-                # cursor = conn.cursor()
-                # TODO User should have to reauthenticate before deletion
-                # Delete user's data from related tables
-                conn.execute("DELETE FROM REGISTRATION WHERE USER_ID = ?", (user_id,))
-                # You may need additional delete operations for related tables, such as items, folders, etc.
-
-                conn.commit()
-                flash("Your account has been successfully deleted.")
-                # Clear the session
-                session.clear()
-                return redirect(url_for("index"))
-
-            except Exception as e:
-                # Handle any errors appropriately
-                print("Error deleting account:", e)
-                flash("Failed to delete your account. Please try again later.")
-
-            finally:
-                if conn:
-                    conn.close()
-        else:
-            # Redirect to login page or handle unauthorized access
-            flash("You are not logged in.")
-        return redirect(url_for("login"))
-
-            # Update your settings HTML template to include a form or button to trigger the account deletion
-
-        return app
-
-        def generate_password(
-                length, min_length, min_numbers, min_special_chars, special_chars, avoid_ambiguous
-        ):
-            password = ""
-            numbers = string.digits
-            special_characters = "".join(special_chars)
-            ambiguous_characters = "0Oo1Il|"
-            if avoid_ambiguous:
-                characters = "".join(
-                    [
-                        c
-                        for c in string.ascii_letters + numbers + special_characters
-                        if c not in ambiguous_characters
-                    ]
+                password = generate_password(
+                    length,
+                    min_length,
+                    min_numbers,
+                    min_special_chars,
+                    special_chars,
+                    avoid_ambiguous,
                 )
-            else:
-                characters = string.ascii_letters + numbers + special_characters
+                return jsonify(password=password)
 
-            # Add minimum numbers
-            for _ in range(min_numbers):
-                password += random.choice(numbers)
+            @app.route("/", methods=["GET", "POST"])
+            @login_required
+            def index():
+                if request.method == "POST":
+                    return handle_generate_password()
 
-            # Add minimum special characters
-            for _ in range(min_special_chars):
-                password += random.choice(special_characters)
+                return render_template("index.html")
 
-            # Generate the rest of the password
-            remaining_length = length - len(password)
-            for _ in range(remaining_length):
-                password += random.choice(characters)
+            @app.route("/gen_Password")
+            @login_required
+            def gen_Password():
+                return render_template("gen_Password.html")
 
-            password = "".join(random.sample(password, len(password)))
-            return password
+            @app.route("/password_generator")
+            @login_required
+            def password_generator():
+                return render_template("Password_generator.html")
+
+            @app.route("/settings")
+            def settings():
+                return render_template("settings.html")
+
+            # Add this route to handle account deletion// not deleting data related to user just user profile
+            @app.route("/delete_account", methods=["POST"])
+            @login_required
+            def delete_account():
+                # Check if the user is authenticated
+                if "user_id" in session:
+                    user_id = session["user_id"]
+                    conn = db.get_db()
+                    try:
+                        # Connect to the database
+                        # conn = database.connect(db_file)
+                        # cursor = conn.cursor()
+                        # TODO User should have to reauthenticate before deletion
+                        # Delete user's data from related tables
+                        conn.execute("DELETE FROM REGISTRATION WHERE USER_ID = ?", (user_id,))
+                        # You may need additional delete operations for related tables, such as items, folders, etc.
+
+                        conn.commit()
+                        flash("Your account has been successfully deleted.")
+                        # Clear the session
+                        session.clear()
+                        return redirect(url_for("index"))
+
+                    except Exception as e:
+                        # Handle any errors appropriately
+                        print("Error deleting account:", e)
+                        flash("Failed to delete your account. Please try again later.")
+
+                    finally:
+                        if conn:
+                            conn.close()
+                else:
+                    # Redirect to login page or handle unauthorized access
+                    flash("You are not logged in.")
+                return redirect(url_for("login"))
+
+            return app
