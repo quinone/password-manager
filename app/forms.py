2c5ad85ed72929300b8f90284c5b411264af049f
Rachie94 <145667840+Rachie94@users.noreply.github.com>
2024-06-13 20:20:26 +0100

    Add files via upload

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, TextAreaField, SubmitField
+from wtforms import StringField, PasswordField, TextAreaField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length
 from wtforms import SelectField, HiddenField
 
@@ -8,11 +8,13 @@ class NewItemForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired(), Length(1, 50)])
     username = StringField("Username", validators=[Length(0, 250)])
     password = PasswordField("Password")
+    show_password = BooleanField("Show Password")
     uri = StringField("URI", validators=[Length(0, 250)])
     notes = TextAreaField("Notes")
-    folder_select = SelectField("Select Folder", choices=[], coerce=int)
+    folder_select = SelectField("Select Folder", choices=[], coerce=str)
     new_folder_name = StringField("New Folder Name", validators=[Length(0, 50)])
     submit = SubmitField("Submit")
+
 
 
 # Search form
