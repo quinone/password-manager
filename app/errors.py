854259ca5a35d4816fc182892f71a9da0354c034
quinone <eoghanm.quinn@gmail.com>
2024-06-15 10:17:17 +0100

    Refactor templates Removed obsolete templates Grouped error templates into folder

--- app/errors.py
+++ app/errors.py
@@ -2,8 +2,8 @@ from flask import render_template
 
 
 def page_not_found(error):
-    return render_template("404.html"), 404
+    return render_template("errors/404.html"), 404
 
 
 def internal_server_error(error):
-    return render_template("500.html"), 500
+    return render_template("errors/500.html"), 500
